6dc630c9019c1369ed538741a8d941c637737a47
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-07-30 13:10:49 -0300

    M02 - Aula 04 - TypeScript no formulário

--- Módulo 02 - Criando SPAs com React JS/03-formularios/src/pages/Home/index.tsx
+++ Módulo 02 - Criando SPAs com React JS/03-formularios/src/pages/Home/index.tsx
@@ -21,12 +21,24 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
-export function Home() {
-  const { register, handleSubmit, watch, formState } = useForm({
-    resolver: zodResolver(newCycleFormValidationSchema),
-  })
+// interface newCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
 
-  function handleCreateNewCycle(data: any) {
+type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function Home() {
+  const { register, handleSubmit, watch, formState } =
+    useForm<newCycleFormData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+        task: '',
+        minutesAmount: 0,
+      },
+    })
+
+  function handleCreateNewCycle(data: newCycleFormData) {
     console.log(data)
   }
 

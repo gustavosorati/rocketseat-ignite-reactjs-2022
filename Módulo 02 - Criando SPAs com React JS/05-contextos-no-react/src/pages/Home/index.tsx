c758c111c480c481ac1e056ad8984718edbe1448
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-08-06 17:38:27 -0300

    M02_cap05 Aula 01 - Convertendo para contexto

---
+++ Módulo 02 - Criando SPAs com React JS/05-contextos-no-react/src/pages/Home/index.tsx
@@ -0,0 +1,114 @@
+import { HandPalm, Play } from 'phosphor-react'
+
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
+import { createContext, useState } from 'react'
+import { NewCycleForm } from '../../components/NewCycleForm'
+import { Countdown } from '../../components/Countdown'
+
+interface ICycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+interface CyclesContextType {
+  activeCycle: ICycle | undefined
+  activeCycleId: string | null
+  markCurrentCycleAsFineshed: () => void
+}
+
+export const CyclesContext = createContext<CyclesContextType>(
+  {} as CyclesContextType,
+)
+
+export function Home() {
+  const [cycles, setCycles] = useState<ICycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  function markCurrentCycleAsFineshed() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, finishedDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+  }
+
+  // function handleCreateNewCycle(data: newCycleFormData) {
+  //   const id = String(new Date().getTime())
+
+  //   const newCycle: ICycle = {
+  //     id,
+  //     task: data.task,
+  //     minutesAmount: data.minutesAmount,
+  //     startDate: new Date(),
+  //   }
+
+  //   setCycles((state) => [...state, newCycle])
+  //   setActiveCycleId(id)
+  //   setAmountSecondsPassed(0)
+
+  //   reset()
+  // }
+
+  function handleInterruptCycle() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+
+    setActiveCycleId(null)
+  }
+
+  // const task = watch('task')
+  // const isSubmitDisable = !task
+
+  return (
+    <HomeContainer>
+      <form /* onSubmit={handleSubmit(handleCreateNewCycle)} */>
+        <CyclesContext.Provider
+          value={{
+            activeCycle,
+            activeCycleId,
+            markCurrentCycleAsFineshed,
+          }}
+        >
+          <NewCycleForm />
+
+          <Countdown />
+
+          {activeCycle ? (
+            <StopCountdownButton type="button" onClick={handleInterruptCycle}>
+              <HandPalm size={24} />
+              Interromper
+            </StopCountdownButton>
+          ) : (
+            <StartCountdownButton
+              /* disabled={isSubmitDisable} */ type="submit"
+            >
+              <Play size={24} />
+              Começar
+            </StartCountdownButton>
+          )}
+        </CyclesContext.Provider>
+      </form>
+    </HomeContainer>
+  )
+}

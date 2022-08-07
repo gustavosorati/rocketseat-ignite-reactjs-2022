09cff6c0fbe25910b524f756591454de9f8d91d8
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-08-06 23:34:00 -0300

    M02_cap06 Aula 05 - Separando Actions

--- Módulo 02 - Criando SPAs com React JS/06-reducers/src/context/CyclesContext.tsx
+++ Módulo 02 - Criando SPAs com React JS/06-reducers/src/context/CyclesContext.tsx
@@ -1,5 +1,11 @@
 import { createContext, ReactNode, useReducer, useState } from 'react'
-import { ActionTypes, cyclesReducer, ICycle } from '../reducers/cycles'
+import {
+  ActionTypes,
+  addNewCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFineshedAction,
+} from '../reducers/cycles/actions'
+import { cyclesReducer, ICycle } from '../reducers/cycles/reducer'
 
 interface CreateCycleData {
   task: string
@@ -49,25 +55,16 @@ export function CyclesContextProvider({
       startDate: new Date(),
     }
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: { newCycle },
-    })
+    dispatch(addNewCycleAction)
     setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: { activeCycleId },
-    })
+    dispatch(interruptCurrentCycleAction)
   }
 
   function markCurrentCycleAsFineshed() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: { activeCycleId },
-    })
+    dispatch(markCurrentCycleAsFineshedAction)
   }
 
   function setSecondsPassed(seconds: number) {

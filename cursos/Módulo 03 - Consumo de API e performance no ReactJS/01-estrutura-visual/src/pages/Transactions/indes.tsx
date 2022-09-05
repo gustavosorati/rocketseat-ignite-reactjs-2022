ed57d240a52e557eb965ada1cce0b807a01075bd
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-05 19:50:53 -0300

    M03_C01 Aula 04 - Tabela de Transações

--- cursos/Módulo 03 - Consumo de API e performance no ReactJS/01-estrutura-visual/src/pages/Transactions/indes.tsx
+++ cursos/Módulo 03 - Consumo de API e performance no ReactJS/01-estrutura-visual/src/pages/Transactions/indes.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header/indes";
 import { Summary } from "../../components/Summary/indes";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
@@ -7,6 +8,33 @@ export function Transactions() {
             <Header />
 
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }

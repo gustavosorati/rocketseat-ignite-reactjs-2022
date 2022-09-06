7fd31aa43eac72a7e8a8dc60745b5ad68c7c2e98
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-05 23:25:21 -0300

    M03_C02 Aula 09 - Buscando transações

--- cursos/Módulo 03 - Consumo de API e performance no ReactJS/02-conexao-com-api/src/pages/Transactions/components/SearchForm/index.tsx
+++ cursos/Módulo 03 - Consumo de API e performance no ReactJS/02-conexao-com-api/src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { MagnifyingGlass} from 'phosphor-react'
 import { useForm } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod"
 import * as z from 'zod';
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
     query: z.string()
@@ -11,6 +13,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+    const { fetchTransactions } = useContext(TransactionsContext)
+
     const { 
         register, 
         handleSubmit,
@@ -23,8 +27,9 @@ export function SearchForm() {
     })
 
     async function handleSearchTransactions(data: SearchFormInputs) {
-        await new Promise(resolve => setTimeout(resolve, 200))
-        console.log(data)
+        await fetchTransactions(data.query)
+        
+
     }
 
     return (

3ca6980946fde8e70e53a89e22ddad1f1de82ed0
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-05 21:38:58 -0300

    M03_C01 Aula 07 - Modal de nova transação

--- cursos/Módulo 03 - Consumo de API e performance no ReactJS/01-estrutura-visual/src/components/Header/indes.tsx
+++ cursos/Módulo 03 - Consumo de API e performance no ReactJS/01-estrutura-visual/src/components/Header/indes.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from '../../assets/logo.svg'
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal/indes";
 
 
 export function Header() {
@@ -14,15 +15,7 @@ export function Header() {
                         <NewTransactionButton>Nova Transação</NewTransactionButton>
                     </Dialog.Trigger>
 
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
-                        <Dialog.Content>
-                            <Dialog.Title>Nova transação</Dialog.Title>
-                        
-                            <Dialog.Close />
-                        </Dialog.Content>
-                    </Dialog.Portal>
-                    
+                    <NewTransactionModal />
                 </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>

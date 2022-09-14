b7f102ab9868e1afecbe6c35ae7d0d02cbdde2dd
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-14 20:51:54 -0300

    Chapter 04 M02_02 - Estilos globais

--- cursos/Módulo 04 - Fundamentos do Next.js/02-estrutura-visual/src/styles/index.ts
+++ cursos/Módulo 04 - Fundamentos do Next.js/02-estrutura-visual/src/styles/index.ts
@@ -4,7 +4,16 @@ import { createStitches } from "@stitches/react";
 export const { config, styled, globalCss, keyframes, getCssText, theme, createTheme, css} = createStitches({
   theme: {
     colors: {
-      rocketseat: '#8257e6'
+      rocketseat: '#8257e6',
+      white: '#FFF',
+
+      gray100: '#e1e1e6',
+      gray300: '#c4c4cc',
+      gray800: '#202024',
+      gray900: '#121214',
+
+      green300: '#00b37e',
+      green500: '#00875f',
     }
   }
 })

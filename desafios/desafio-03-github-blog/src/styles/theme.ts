a479281bace53cb12c98b657c930657756353695
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-17 13:57:18 -0300

    Definição das propriedades do tema

---
+++ desafios/desafio-03-github-blog/src/styles/theme.ts
@@ -0,0 +1,25 @@
+export const defaultTheme = {
+  colors: {
+    blue: "#3294f8",
+
+    "base-title": "#e7edf4",
+    "base-subtitle": "#C4D4E3",
+    "base-text": "#AFC2D4",
+    "base-span": "#7B96B2",
+    "base-label": "#3A536B",
+    "base-border": "#1C2F41",
+    "base-post": "#112131",
+    "base-profile": "#0B1B2B",
+    "base-background": "#071422",
+    "base-input": "#040F1A",
+  },
+
+  fonts: {
+    small: '0.75rem',
+    medium: '0.875rem',
+    xl: '1rem',
+    '2xl': '1.125rem',
+    '3xl': '1.25rem',
+    '4xl': '1.50rem',
+  }
+}

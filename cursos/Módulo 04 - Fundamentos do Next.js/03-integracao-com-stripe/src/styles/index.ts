1b18d47c99c676872d746ebacdc6d8a1429d9f3a
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-19 20:02:01 -0300

    Chapter 04 M3_01 - Configurando conta Stripe

---
+++ cursos/Módulo 04 - Fundamentos do Next.js/03-integracao-com-stripe/src/styles/index.ts
@@ -0,0 +1,27 @@
+import { createStitches } from "@stitches/react";
+
+
+export const { config, styled, globalCss, keyframes, getCssText, theme, createTheme, css} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6',
+      white: '#FFF',
+
+      gray100: '#e1e1e6',
+      gray300: '#c4c4cc',
+      gray800: '#202024',
+      gray900: '#121214',
+
+      green300: '#00b37e',
+      green500: '#00875f',
+    },
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    }
+  }
+})
+
+

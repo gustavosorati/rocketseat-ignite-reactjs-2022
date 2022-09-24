b79de0e904f41132e846819055ccbc24d835f272
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-23 21:28:45 -0300

    projeto github-blog

--- desafios/desafio-03-github-blog/src/pages/Post/components/PostContent/styles.ts
+++ desafios/desafio-03-github-blog/src/pages/Post/components/PostContent/styles.ts
@@ -11,8 +11,10 @@ export const PostContentContaienr = styled.div`
     width: 100%;
   }
 
-  code {
+  pre {
     background-color: ${({theme}) => theme.colors["base-post"]};
+    padding: 1rem;
+    border-radius: 4px;
   }
 
 `
